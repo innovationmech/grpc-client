3927d6b52f816342a8b6d193dcb61281991809be
jackelyj <jackelyj@gmail.com>
2023-07-04 01:01:46 +0800

    refactor

--- cmd/root.go
+++ cmd/root.go
@@ -19,67 +19,33 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package cmd
+package main
 
 import (
-	"fmt"
 	"os"
 
+	"github.com/innovationmech/grpc-client/internal/controller"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var rootCmd = newRootCmd()
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "grpc-client",
-	Short: "A brief description of your application",
+func newRootCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "grpc-client",
+		Short: "A brief description of your application",
+	}
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func main() {
+	newCmdFuncs := []func() *cobra.Command{
+		controller.NewServeCmd,
+	}
+	for _, newCmdFunc := range newCmdFuncs {
+		rootCmd.AddCommand(newCmdFunc())
+	}
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.grpc-client.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".grpc-client" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".grpc-client")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
--- internal/controller/web.go
+++ internal/controller/web.go
@@ -1,20 +1,56 @@
 package controller
 
 import (
+	"context"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/innovationmech/grpc-client/internal/middleware"
-	_ "github.com/innovationmech/grpc-client/internal/pool"
+	"github.com/innovationmech/grpc-client/internal/pool"
+	"github.com/innovationmech/grpc-client/pb"
+	"github.com/spf13/cobra"
 )
 
 var r = gin.Default()
 
-func Serve() {
-	r.Run(":8080")
+func NewServeCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "serve",
+		Short: "A brief description of your command",
+		Run: func(cmd *cobra.Command, args []string) {
+			r.Use(middleware.LimitMiddleware())
+			r.Use(middleware.TimerMiddleware())
+			r.GET("/health", health)
+			r.GET("/dial_hello", dial)
+			r.Run(":8080")
+		},
+	}
 }
 
-func init() {
-	r.Use(middleware.LimitMiddleware())
-	r.Use(middleware.TimerMiddleware())
-	r.GET("/health", health)
-	r.GET("/dial_hello", dial)
+func health(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func dial(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": hello(),
+	})
+}
+
+func hello() string {
+	cp := pool.GetDefaultConnPool()
+	conn := cp.Get()
+	if conn == nil {
+		return ""
+	}
+	c := pb.NewHelloServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Greeting: "world"})
+	if err != nil {
+		return err.Error()
+	}
+	return r.Reply
 }
